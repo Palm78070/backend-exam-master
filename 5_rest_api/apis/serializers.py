4f81858250c24aa41c0bb9cdade299a69dd285dd
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2024-07-12 00:15:01 +0700

    fix(serializer): School serializer can show detail of teacher/student count

--- 5_rest_api/apis/serializers.py
+++ 5_rest_api/apis/serializers.py
@@ -5,14 +5,25 @@ from .models import School, Classroom, Teacher, Student
 
 class SchoolSerializer(serializers.ModelSerializer):
 	#source specifies where the serializer should look to retrieve data for the field
-	classrooms_count = serializers.IntegerField(source='classrooms.count', read_only=True) #classrooms is the related name in the School model .count() method from Django ORM
-	teachers_count = serializers.IntegerField(source='classrooms__teachers.count', read_only=True)
-	students_count = serializers.IntegerField(source='classrooms__students.count', read_only=True)
+	# classrooms_count = serializers.IntegerField(source='classrooms.count', read_only=True) #classrooms is the related name in the School model .count() method from Django ORM
+	# teachers_count = serializers.IntegerField(source='classrooms__teachers.count', read_only=True)
+	# students_count = serializers.IntegerField(source='classrooms__students.count', read_only=True)
+
+	classrooms_count = serializers.SerializerMethodField()
+	teachers_count = serializers.SerializerMethodField()
+	students_count = serializers.SerializerMethodField()
 
 	class Meta:
 		#model and fields that we want to serialize
 		model = School
 		fields = '__all__'
+
+	def get_classrooms_count(self, obj):
+		return obj.classrooms.count()
+	def get_teachers_count(self, obj):
+		return Teacher.objects.filter(classrooms__school=obj).distinct().count() #This method use a query to count distinct Teacher obj relate to School => Teacher is get from Classroom that related to School
+	def get_students_count(self, obj):
+		return Student.objects.filter(classroom__school=obj).distinct().count() #This method use a query to count distinct Student obj relate to School => Student is get from Classroom that related to School
 
 class ClassroomSerializer(serializers.ModelSerializer):
 	#StringRelatedField is typically used when you want to represent a related model instance as a string (usually the __str__ representation of the related object).
